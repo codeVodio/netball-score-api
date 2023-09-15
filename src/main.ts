30c4726acb8ea151cb267b1cbf3a30483e70dd6f
codeVodio <codevodio@gmail.com>
2023-09-15 14:50:34 +0200

    added cors(default) for local frontend development and swagger for documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true});
+
+  const config = new DocumentBuilder()
+  .setTitle('Malawi netball scores')
+  .setDescription('A simple api for keeping netball games scores')
+  .setVersion('1.0')
+  .addTag('cats')
+  .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(4000);
 }
 bootstrap();
